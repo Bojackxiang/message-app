e94c2489660119adf17bddb838740b528e023413
Alex <alex@q-83.com>
2023-05-11 12:02:30 +1000

    update avatar

--- app/users/components/Avatar.tsx
+++ app/users/components/Avatar.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { User } from "@prisma/client";
 
@@ -7,34 +7,36 @@ import Image from "next/image";
 
 interface AvatarProps {
   user?: User | null;
-};
+}
 
 const Avatar: React.FC<AvatarProps> = ({ user }) => {
   // const { members } = useActiveList();
   // const isActive = members.indexOf(user?.email!) !== -1;
-  const isActive = true; 
-  console.log("user", user)
+  const isActive = true;
+  console.log("user", user);
 
   return (
     <div className="relative">
-      <div className="
-        relative 
-        inline-block 
-        rounded-full 
-        overflow-hidden
-        h-9 
-        w-9 
-        md:h-11 
-        md:w-11
-      ">
-        <Image
-          fill
-          src={user?.image || '/images/placeholder.jpg'}
-          alt="Avatar"
-        />
-      </div>
+      {user?.name ? (
+        <>
+          <div className="relative inline-block rounded-full overflow-hiddenh-9 w-9 md:h-11 md:w-11 bg-red-300 text-center">
+            <span className="block text-xl mt-2">{user.name[0].toUpperCase()}</span>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="relative inline-block rounded-full overflow-hiddenh-9 w-9 md:h-11 md:w-11">
+            <Image
+              fill
+              src={user?.image || "/images/placeholder.jpg"}
+              alt="Avatar"
+            />
+          </div>
+        </>
+      )}
+
       {isActive ? (
-        <span 
+        <span
           className="
             absolute 
             block 
@@ -48,11 +50,11 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
             w-2 
             md:h-3 
             md:w-3
-          " 
+          "
         />
       ) : null}
     </div>
   );
-}
+};
 
 export default Avatar;
